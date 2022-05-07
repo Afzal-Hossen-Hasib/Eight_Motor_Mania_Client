f071f0897c26620bcf5ef17ead1794f35002a356
afzalhossenhasib <ahhasib47@gmail.com>
2022-05-08 04:30:04 +0600

    verification added

--- src/Pages/Login/PrivateRoute/PrivateRoute.js
+++ src/Pages/Login/PrivateRoute/PrivateRoute.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../SharedPages/Loading/Loading';
 
@@ -8,6 +9,7 @@ import Loading from '../../SharedPages/Loading/Loading';
 const PrivateRoute = ({children}) => {
     const [user, loading] = useAuthState(auth);
     const location = useLocation();
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
 
     if (loading) {
         return <Loading></Loading>
@@ -16,6 +18,25 @@ const PrivateRoute = ({children}) => {
     if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
+
+    if (user.providerData[0]?.providerId ==='password' && !user.emailVerified) {
+        return <div className='text-center mt-5'>
+            <h3 className='text-danger'>Your Email is not verified!!</h3>
+            <h5 className='text-success'> Please Verify your email address</h5>
+            <button
+            className='update-button d-block mx-auto'
+                onClick={async () => {
+                    await sendEmailVerification();
+                    toast('Verification Email Sent');
+                }}
+            >
+                Send Again
+            </button>
+            <ToastContainer></ToastContainer>
+        </div>
+    }
+
+
     return children;
 };
 
